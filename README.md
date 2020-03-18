fc2feb47604d6d146ed3c234c34e50a1fbda8f52
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-03-18 16:50:16 +0100

    Initialization - first commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository contains retro style website project created from PSD.
+
+Used technologies: HTML, SCSS, JS;
+
+Used tools: VS Code, Gulp, Adobe Photoshop.
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+// compile scs into css
+function style() {
+  // 1. where is my scss file
+  return gulp.src('./src/scss/**/*.scss')
+  // 2. pass that file through sass compiler
+    .pipe(sass(/* {
+      outputStyle : "extended" //code style - extended, nested, copressed, compact
+    } */).on('error', sass.logError))
+  // Use autoprefixer
+    .pipe(autoprefixer())
+  // 3. where do I save the compiled CSS?
+    .pipe(gulp.dest('./dist/style'))
+  // 4. stream changes to all browser
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+      directory: true
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);
+  gulp.watch('./dist/script/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
