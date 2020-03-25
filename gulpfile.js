bce0f6bcd865915b2598b10ddf921804dc1bd4cf
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-03-25 10:25:19 +0100

    Clear up commit.
    I've decided to change approach to make this project. I'll do it in more
    coherent way.

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,12 @@ const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
-// compile scs into css
 function style() {
-  // 1. where is my scss file
   return gulp.src('./src/scss/**/*.scss')
-  // 2. pass that file through sass compiler
     .pipe(sass({
-      outputStyle : "extended" //code style - extended, nested, copressed, compact
     }).on('error', sass.logError))
-  // Use autoprefixer
     .pipe(autoprefixer())
-  // 3. where do I save the compiled CSS?
     .pipe(gulp.dest('./dist/style'))
-  // 4. stream changes to all browser
     .pipe(browserSync.stream());
 }
 
