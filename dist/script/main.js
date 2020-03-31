c523a79f4e2ed65d87da198a289b6de4f7779385
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-03-31 13:17:35 +0200

    Add 'Go up button'.

--- dist/script/main.js
+++ dist/script/main.js
@@ -8,3 +8,38 @@ navigationButton.addEventListener('click', function() {
   navigationLinkNode.forEach(link => link.classList.toggle('open'));
   navigationLinks.classList.toggle('open');
 });
+
+
+const upButton = document.querySelector('.up-button');
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkButton(e) {
+  const main = document.querySelector('.main');
+  const mainTop = main.offsetTop;
+  const windowScrolledY = window.scrollY;
+  if (windowScrolledY > mainTop) {
+    upButton.classList.add('active');
+  } else {
+    upButton.classList.remove('active');
+  };
+};
+
+window.addEventListener('scroll', debounce(checkButton));
+
+upButton.addEventListener('click', function() {
+  window.scrollTo({top: 0, behavior:'smooth'})
+});
